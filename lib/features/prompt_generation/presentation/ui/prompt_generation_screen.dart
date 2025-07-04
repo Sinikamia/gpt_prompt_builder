30ab6f61747eebc85e7f21149984df143147d356
Sinikamia <nastuska.089601@gmail.com>
2025-07-04 16:51:14 +0300

    Update prompt_generation_screen.dart

--- lib/features/prompt_generation/presentation/ui/prompt_generation_screen.dart
+++ lib/features/prompt_generation/presentation/ui/prompt_generation_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:gpt_prompt_builder/constants/prompt_category.dart';
+import 'package:gpt_prompt_builder/constants/prompt_subcategory.dart';
+import 'package:gpt_prompt_builder/features/prompt_generation/domain/models/prompt_subcategory_models.dart';
 import 'package:gpt_prompt_builder/features/prompt_generation/presentation/ui/widgets/button_prompt_generation.dart';
 import 'package:gpt_prompt_builder/features/prompt_generation/presentation/ui/widgets/category_universal.dart';
 import 'package:gpt_prompt_builder/features/prompt_generation/presentation/ui/widgets/text_field_universal.dart';
@@ -13,6 +16,187 @@ class PromptGenerationScreen extends StatefulWidget {
 }
 
 class _PromptGenerationScreenState extends State<PromptGenerationScreen> {
+  final GlobalKey _CategorymenuKey = GlobalKey();
+  final GlobalKey _SubCategorymenuKey = GlobalKey();
+  late String selectedCategory;
+  bool _isMenuOpenCategory = false;
+  bool _isMenuOpenSubCategory = false;
+  late String selectedSubCategory;
+
+  @override
+  void initState() {
+    super.initState();
+    selectedCategory = widget.selectedCategory;
+    final subList = getSubCategoriesByCategory(selectedCategory);
+    selectedSubCategory = subList.isNotEmpty ? subList.first : '';
+  }
+
+  List<String> getSubCategoriesByCategory(String category) {
+    return subCategories
+        .firstWhere(
+          (e) => e.category == category,
+          orElse: () => PromptSubCategory(category: '', subCategory: []),
+        )
+        .subCategory;
+  }
+
+  void _showDropdownMenu(BuildContext context) async {
+    setState(() {
+      _isMenuOpenCategory = true;
+    });
+    final RenderBox renderBox =
+        _CategorymenuKey.currentContext!.findRenderObject() as RenderBox;
+    final Offset offset = renderBox.localToGlobal(Offset.zero);
+    final size = renderBox.size;
+    final screenSize = MediaQuery.of(context).size;
+    final double menuHeight = promptCategories.length * 48.0;
+    final double desiredWidth = 200;
+    final double menuWidth = desiredWidth.clamp(150.0, 300.0);
+    final double top = offset.dy - menuHeight;
+    final double left = offset.dx + size.width - 200;
+    final double right = screenSize.width - offset.dx - size.width;
+    final double bottom = screenSize.height - offset.dy;
+    final selected = await showMenu<String>(
+      context: context,
+      position: RelativeRect.fromLTRB(left, top, right, bottom),
+      items: [
+        PopupMenuItem<String>(
+          enabled: false,
+          child: SizedBox(
+            width: menuWidth,
+            height: promptCategories.length * 47.0,
+            child: ListView(
+              padding: EdgeInsets.zero,
+              shrinkWrap: true,
+              children:
+                  promptCategories.map((promptCategory) {
+                    return ListTile(
+                      title: Container(
+                        padding: const EdgeInsets.symmetric(vertical: 12),
+                        decoration: BoxDecoration(
+                          border: Border(
+                            bottom: BorderSide(
+                              color: Colors.white24,
+                              width: 0.3,
+                            ),
+                          ),
+                        ),
+                        child: Center(
+                          child: Text(
+                            promptCategory.category,
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              color: Colors.white70,
+                              fontSize: 14,
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                        ),
+                      ),
+                      onTap: () {
+                        Navigator.pop(context, promptCategory.category);
+                      },
+                    );
+                  }).toList(),
+            ),
+          ),
+        ),
+      ],
+      elevation: 4,
+      color: Color(0xFF1C1C1E),
+    );
+    setState(() {
+      _isMenuOpenCategory = false;
+    });
+    if (selected != null) {
+      final subList = getSubCategoriesByCategory(selected);
+      setState(() {
+        selectedCategory = selected;
+        selectedSubCategory = subList.isNotEmpty ? subList.first : '';
+      });
+    }
+  }
+
+  void _showDropdownMenuSubCategory(BuildContext context) async {
+    setState(() {
+      _isMenuOpenSubCategory = true;
+    });
+    final RenderBox renderBox =
+        _SubCategorymenuKey.currentContext!.findRenderObject() as RenderBox;
+    final Offset offset = renderBox.localToGlobal(Offset.zero);
+    final size = renderBox.size;
+    final screenSize = MediaQuery.of(context).size;
+    final subCategoryList = getSubCategoriesByCategory(selectedCategory);
+    if (subCategoryList.isEmpty) {
+      setState(() => _isMenuOpenSubCategory = false);
+      return;
+    }
+    final double menuHeight = subCategoryList.length * 48.0;
+    final double desiredWidth = 200;
+    final double menuWidth = desiredWidth.clamp(150.0, 300.0);
+    final double top = offset.dy - menuHeight;
+    final double left = offset.dx + size.width - 200;
+    final double right = screenSize.width - offset.dx - size.width;
+    final double bottom = screenSize.height - offset.dy;
+    final selected = await showMenu<String>(
+      context: context,
+      position: RelativeRect.fromLTRB(left, top, right, bottom),
+      items: [
+        PopupMenuItem<String>(
+          enabled: false,
+          child: SizedBox(
+            width: menuWidth,
+            height: subCategoryList.length * 47.0,
+            child: ListView(
+              padding: EdgeInsets.zero,
+              shrinkWrap: true,
+              children:
+                  subCategoryList.map((subCategory) {
+                    return ListTile(
+                      title: Container(
+                        padding: const EdgeInsets.symmetric(vertical: 12),
+                        decoration: BoxDecoration(
+                          border: Border(
+                            bottom: BorderSide(
+                              color: Colors.white24,
+                              width: 0.3,
+                            ),
+                          ),
+                        ),
+                        child: Center(
+                          child: Text(
+                            subCategory,
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              color: Colors.white70,
+                              fontSize: 14,
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                        ),
+                      ),
+                      onTap: () {
+                        Navigator.pop(context, subCategory);
+                      },
+                    );
+                  }).toList(),
+            ),
+          ),
+        ),
+      ],
+      elevation: 4,
+      color: Color(0xFF1C1C1E),
+    );
+    setState(() {
+      _isMenuOpenSubCategory = false;
+    });
+    if (selected != null) {
+      setState(() {
+        selectedSubCategory = selected;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -51,14 +235,24 @@ class _PromptGenerationScreenState extends State<PromptGenerationScreen> {
             ),
             SizedBox(height: 20),
             CategoryUniversal(
+              key: _CategorymenuKey,
               text: 'Категория',
-              textCategory: 'Категория',
-              onTap: () {},
+              textCategory: selectedCategory,
+              icon:
+                  _isMenuOpenCategory
+                      ? Icons.arrow_drop_up
+                      : Icons.arrow_drop_down,
+              onTap: () => _showDropdownMenu(context),
             ),
             CategoryUniversal(
+              key: _SubCategorymenuKey,
               text: 'Подкатегория',
-              textCategory: 'Подкатегория',
-              onTap: () {},
+              textCategory: selectedSubCategory,
+              icon:
+                  _isMenuOpenSubCategory
+                      ? Icons.arrow_drop_up
+                      : Icons.arrow_drop_down,
+              onTap: () => _showDropdownMenuSubCategory(context),
             ),
             SizedBox(height: 20),
             ButtonPromptGeneration(text: 'Сгенерировать промпт', onTap: () {}),
